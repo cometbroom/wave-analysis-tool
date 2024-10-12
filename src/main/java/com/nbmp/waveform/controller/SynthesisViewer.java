38a8d64144bdcae9f5b5ef4e15dc981f4ffc4196
alawi <alawi.wow@gmail.com>
2024-10-13 01:03:44 +0200

    Added a simple "independent form of synthesis"

--- src/main/java/com/nbmp/waveform/controller/SynthesisViewer.java
+++ src/main/java/com/nbmp/waveform/controller/SynthesisViewer.java
@@ -1,36 +1,48 @@
+/* (C)2024 */
 package com.nbmp.waveform.controller;
 
-import com.nbmp.waveform.model.dto.BiTimeSeries;
 import com.nbmp.waveform.model.dto.WavePropsSliders;
 import com.nbmp.waveform.model.generation.Synthesis;
 import com.nbmp.waveform.view.WavesRegister;
-import lombok.RequiredArgsConstructor;
 
-import java.util.function.Consumer;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @RequiredArgsConstructor
+@Getter
+@Setter
 public class SynthesisViewer {
-    private final WavesRegister wave1, wave2;
-    private final Synthesis synthesis;
-    private final int duration;
+  private final WavesRegister wave1, wave2;
+  private Synthesis synthesis;
+  private final int duration;
 
-    public void synthesizeForPair(WavePropsSliders slider1, WavePropsSliders slider2) {
-        regenSeriesData(duration);
-        setUpdateTask(slider1);
-        setUpdateTask(slider2);
-    }
+  public SynthesisViewer(
+      WavesRegister wave1, WavesRegister wave2, Synthesis synthesis, int duration) {
+    this.wave1 = wave1;
+    this.wave2 = wave2;
+    this.synthesis = synthesis;
+    this.duration = duration;
+  }
 
-    private void regenSeriesData(int duration) {
-        var newData = synthesis.compute(duration);
-        wave1.refreshData(newData.timeAmplitude1());
-        wave2.refreshData(newData.timeAmplitude2());
-    }
+  public void synthesizeForPair(WavePropsSliders slider1, WavePropsSliders slider2) {
+    regenSeriesData(duration);
+    setUpdateTask(slider1);
+    setUpdateTask(slider2);
+  }
 
-    private void setUpdateTask(WavePropsSliders slider) {
-        slider.setUpdateTask((newValue) -> {
-            var newData = synthesis.compute(duration);
-            wave1.refreshData(newData.timeAmplitude1());
-            wave2.refreshData(newData.timeAmplitude2());
+  private void regenSeriesData(int duration) {
+    var newData = synthesis.compute(duration);
+    wave1.refreshData(newData.timeAmplitude1());
+    wave2.refreshData(newData.timeAmplitude2());
+  }
+
+  private void setUpdateTask(WavePropsSliders slider) {
+    slider.setUpdateTask(
+        (newValue) -> {
+          var newData = synthesis.compute(duration);
+          wave1.refreshData(newData.timeAmplitude1());
+          wave2.refreshData(newData.timeAmplitude2());
         });
-    }
+  }
 }
--- src/main/java/com/nbmp/waveform/controller/WaveController.java
+++ src/main/java/com/nbmp/waveform/controller/WaveController.java
@@ -3,30 +3,32 @@ package com.nbmp.waveform.controller;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.function.Consumer;
-
-import com.nbmp.waveform.model.dto.WavePropsSliders;
-import com.nbmp.waveform.view.WavesRegister;
-import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.*;
-import javafx.util.Duration;
 
 import org.springframework.stereotype.Component;
 
-import com.nbmp.waveform.model.dto.BiTimeSeries;
+import com.nbmp.waveform.model.dto.WavePropsSliders;
 import com.nbmp.waveform.model.generation.ChaosSynthesis;
+import com.nbmp.waveform.model.generation.IndependentSynthesis;
+import com.nbmp.waveform.model.generation.Synthesis;
+import com.nbmp.waveform.view.WavesRegister;
 
 @Component
 public class WaveController {
   public Label statusLabel;
   public LineChart<Number, Number> resultWaveformChart;
+  @FXML public ComboBox<String> synthesisMode;
   @FXML private Slider frequencySlider;
   @FXML public Slider frequencySlider2;
   @FXML private Label sliderLabel;
   @FXML private Label sliderLabel2;
   @FXML private LineChart<Number, Number> waveformChart;
+
+  private SynthesisViewer synthesisViewer;
+  private WavePropsSliders slider1;
+  private WavePropsSliders slider2;
 
   private List<WavesRegister> waves = new LinkedList<>();
 
@@ -45,13 +47,32 @@ public class WaveController {
         WavesRegister.createWaveform("sine1", WaveType.SINE, 5, 1).addToChart(waveformChart);
     var sineWave2 =
         WavesRegister.createWaveform("sine2", WaveType.SINE, 10, 1).addToChart(resultWaveformChart);
-
+    Synthesis synthesis = new IndependentSynthesis(sineWave, sineWave2);
     int duration = 1;
 
-    var chaosSytnthesis = new ChaosSynthesis(sineWave, sineWave2);
-    var synthesisViewer = new SynthesisViewer(sineWave, sineWave2, chaosSytnthesis, duration);
-    var slider1 = new WavePropsSliders(sineWave, frequencySlider, sliderLabel);
-    var slider2 = new WavePropsSliders(sineWave2, frequencySlider2, sliderLabel2);
+    synthesisMode.getItems().addAll("Independent", "Chaos");
+    synthesisMode.getSelectionModel().select(0);
+    synthesisMode
+        .getSelectionModel()
+        .selectedItemProperty()
+        .addListener(
+            (observableValue, s, t1) -> {
+              if (t1.equals("Independent")) {
+                IndependentSynthesis independentSynthesis =
+                    new IndependentSynthesis(sineWave, sineWave2);
+                synthesisViewer.setSynthesis(independentSynthesis);
+                synthesisViewer.synthesizeForPair(slider1, slider2);
+              }
+              if (t1.equals("Chaos")) {
+                ChaosSynthesis chaosSynthesis = new ChaosSynthesis(sineWave, sineWave2);
+                synthesisViewer.setSynthesis(chaosSynthesis);
+                synthesisViewer.synthesizeForPair(slider1, slider2);
+              }
+            });
+
+    synthesisViewer = new SynthesisViewer(sineWave, sineWave2, synthesis, duration);
+    slider1 = new WavePropsSliders(sineWave, frequencySlider, sliderLabel);
+    slider2 = new WavePropsSliders(sineWave2, frequencySlider2, sliderLabel2);
 
     slider1.addListenerAccordingToTarget(WavePropsSliders.Target.FREQUENCY);
     slider2.addListenerAccordingToTarget(WavePropsSliders.Target.FREQUENCY);
