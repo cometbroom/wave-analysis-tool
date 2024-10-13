97c522a23daa5fa12c600fcfea9812e5888041d8
alawi <alawi.wow@gmail.com>
2024-10-13 21:30:10 +0200

    Simplified Spring FXML context loading

--- src/main/java/com/nbmp/waveform/application/AppConfig.java
+++ src/main/java/com/nbmp/waveform/application/AppConfig.java
@@ -1,38 +1,9 @@
 /* (C)2024 */
 package com.nbmp.waveform.application;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
-import com.nbmp.waveform.controller.ControllersState;
-import com.nbmp.waveform.controller.ChartController;
-import com.nbmp.waveform.controller.WaveController;
-import com.nbmp.waveform.view.WavesRegister;
-
-@ComponentScan(basePackages = "com.nbmp.waveform")
 @Configuration
 public class AppConfig {
   public static final int SAMPLE_RATE = 44100;
-
-  @Bean
-  @Scope("singleton")
-  public ControllersState controllersState() {
-    var sine1 = WavesRegister.createWaveform("sine1", WaveController.WaveType.SINE, 5, 1);
-    var sine2 = WavesRegister.createWaveform("sine2", WaveController.WaveType.SINE, 7, 1);
-    return ControllersState.createInstance(sine1, sine2);
-  }
-
-  @Bean
-  @Scope("prototype")
-  public WaveController waveController() {
-    return new WaveController();
-  }
-
-  @Bean
-  @Scope("prototype")
-  public ChartController chartController() {
-    return new ChartController();
-  }
 }
--- src/main/java/com/nbmp/waveform/application/SpringFXMLLoader.java
+++ src/main/java/com/nbmp/waveform/application/SpringFXMLLoader.java
@@ -2,58 +2,42 @@
 package com.nbmp.waveform.application;
 
 import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.JavaFXBuilderFactory;
-import javafx.scene.Parent;
-import javafx.util.BuilderFactory;
+import javafx.scene.layout.AnchorPane;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
-import lombok.RequiredArgsConstructor;
+import com.nbmp.waveform.controller.ControllersState;
+import com.nbmp.waveform.controller.WaveController;
+import com.nbmp.waveform.view.WavesRegister;
 
-@RequiredArgsConstructor
-public class SpringFXMLLoader<T> {
-  private final URL location;
+@Configuration
+@ComponentScan(basePackages = "com.nbmp.waveform")
+@ComponentScan(basePackages = "com.nbmp.waveform.controller.component")
+public class SpringFXMLLoader {
   private final ApplicationContext applicationContext;
-  private Charset charset = Charset.defaultCharset();
-  private T controller;
 
-  private boolean loaded;
-
-  public Parent load() throws IOException {
-    if (loaded) {
-      throw new IllegalStateException("Cannot load fxml multiple times");
-    }
-    FXMLLoader loader = new FXMLLoader();
-    loader.setCharset(charset);
-    loader.setLocation(location);
-    loader.setControllerFactory(applicationContext::getBean);
-    loader.setBuilderFactory(
-        new BuilderFactory() {
-          JavaFXBuilderFactory defaultFactory = new JavaFXBuilderFactory();
-
-          @Override
-          public javafx.util.Builder<?> getBuilder(Class<?> type) {
-            String[] beanNames = applicationContext.getBeanNamesForType(type);
-            if (beanNames.length == 1) {
-              return (javafx.util.Builder<Object>) () -> applicationContext.getBean(beanNames[0]);
-            } else {
-              return defaultFactory.getBuilder(type);
-            }
-          }
-        });
-    Parent root = loader.load();
-    controller = loader.getController();
-    loaded = true;
-    return root;
+  public SpringFXMLLoader(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
   }
 
-  public T getController() {
-    if (!loaded) {
-      throw new IllegalStateException("Controller is only available after loading");
-    }
-    return controller;
+  @Bean(name = "mainView")
+  public AnchorPane loadMainView() throws IOException {
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("/Application.fxml"));
+    loader.setControllerFactory(applicationContext::getBean);
+    return loader.load();
+  }
+
+  @Bean
+  @Scope("singleton")
+  public ControllersState controllersState() {
+    var sine1 = WavesRegister.createWaveform("sine1", WaveController.WaveType.SINE, 5, 1);
+    var sine2 = WavesRegister.createWaveform("sine2", WaveController.WaveType.SINE, 7, 1);
+    return ControllersState.createInstance(sine1, sine2);
   }
 }
